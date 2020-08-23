dbf3d2645dc207295f84f841cd89d1454c70e7a3
mcinotti <martincinotti@gmail.com>
2020-08-23 12:26:16 -0400

    add -f option to force override of symlink create.

--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,41 +5,41 @@ echo "*** Creating symlinks! ***"
 echo "**************************"
 
 # Silence Login Messages
-ln -s ~/dotfiles/hushlogin ~/.hushlogin
+ln -sf ~/dotfiles/hushlogin ~/.hushlogin
 
 # User Dirs
-ln -s ~/dotfiles/user-dirs.dirs ~/.config/user-dirs.dirs
-ln -s ~dotfiles/user-dirs.locale ~/.config/user-dirs.locale
+ln -sf ~/dotfiles/user-dirs.dirs ~/.config/user-dirs.dirs
+ln -sf ~dotfiles/user-dirs.locale ~/.config/user-dirs.locale
 
 # Shell Configs
-ln -s ~/dotfiles/bashrc ~/.bashrc
-ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -sf ~/dotfiles/bashrc ~/.bashrc
+ln -sf ~/dotfiles/zshrc ~/.zshrc
 
 # Sway
-ln -s ~/dotfiles/sway ~/.config/sway
-ln -s ~/dotfiles/waybar ~/.config/waybar
+ln -sf ~/dotfiles/sway ~/.config/sway
+ln -sf ~/dotfiles/waybar ~/.config/waybar
 
 # Swaylock
-ln -s ~/dotfiles/sway/swaylock ~/.config/swaylock
+ln -sf ~/dotfiles/sway/swaylock ~/.config/swaylock
 
 
 # Xorg resources
-ln -s ~/dotfiles/Xresources.d ~/.Xresources.d
-ln -s ~/dotfiles/Xresources.d/Xresources ~/.Xresources
-ln -s ~/dotfiles/Xresources.d/Xresources ~/.Xdefaults
+ln -sf ~/dotfiles/Xresources.d ~/.Xresources.d
+ln -sf ~/dotfiles/Xresources.d/Xresources ~/.Xresources
+ln -sf ~/dotfiles/Xresources.d/Xresources ~/.Xdefaults
 
 # Themes, Fonts, Icons
-ln -s ~/dotfiles/themes ~/.themes
-ln -s ~/dotfiles/fonts ~/.fonts
-ln -s ~/dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
-ln -s ~/dotfiles/gtk-2.0 ~/.config/gtk-2.0
-ln -s ~/dotfiles/gtk-3.0 ~/.config/gtk-3.0
+ln -sf ~/dotfiles/themes ~/.themes
+ln -sf ~/dotfiles/fonts ~/.fonts
+ln -sf ~/dotfiles/gtkrc-2.0 ~/.gtkrc-2.0
+ln -sf ~/dotfiles/gtk-2.0 ~/.config/gtk-2.0
+ln -sf ~/dotfiles/gtk-3.0 ~/.config/gtk-3.0
 
 # Termite
-ln -s ~/dotfiles/termite ~/.config/termite
+ln -sf ~/dotfiles/termite ~/.config/termite
 
 # Vim setup
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -sf ~/dotfiles/vim ~/.vim
+ln -sf ~/dotfiles/vimrc ~/.vimrc
 
 echo "Finished"
